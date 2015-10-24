672f90f3f6aa326d95e2242993636ed61fcad93a
Loren Barrick <lorenbarrick@gmail.com>
2015-10-24 14:36:12 -0700

    working dealRound

--- blackJack.js
+++ blackJack.js
@@ -15,36 +15,38 @@ function makeDeck() {
   return deck;
 }
 
+function shuffle(cards) { var temp; var random; // shuffle the cards into a random order for (var i = 0; i < cards.length; i++) { temp = cards[i]; random = Math.floor(Math.random() * cards.length); cards[i] = cards[random]; cards[random] = temp; } return cards; }
+}
 
 function shuffle(deck) {
-    for (var i = deck.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
-    return deck;
+ for (var i = deck.length - 1; i > 0; i--) {
+   var j = Math.floor(Math.random() * (i + 1));
+   var temp = deck[i];
+   deck[i] = deck[j];
+   deck[j] = temp;
+ }
+  return deck;
 }
 
 function dealCard(deck) {
   return deck.shift();
 }
+
 //dealer gets last card
 function dealRound(deck, numberOfPlayers) {
   var round = [];
-  var numberOfHands = numberOfPlayers + 1;
-  for (var i = 0; i < numberOfPlayers; i++) {
+  for (var i = 0; i < numberOfPlayers + 1; i++) {
     round.push([]);
   }
-    for (var k = 0; k < 2; k++) {
-      // number is added by 1 to include dealer.
-      for (var j = 1; j < numberOfPlayers; j++) {
-        round[j].push(dealCard(deck));
-      }
-      round[0].push(dealCard(deck));
-    }
-  return round;
+  for (var k = 0; k < 2; k++) {
+    // number is added by 1 to include dealer.
+   for (var j = 1; j <= numberOfPlayers; j++) {
+     round[j].push(dealCard(deck));
+   }
+     round[0].push(dealCard(deck));
   }
+  return round;
+}
 
 function each(hand, callback) {
   for (var i = 0; i < hand.length; i++) {
@@ -56,8 +58,8 @@ function handValue(hand) {
   var score = [0];
   for (var i = 0; i < hand.length; i++) {
 
-    console.log(hand);
-    console.log(hand[0].rank);
+    //console.log(hand);
+    //console.log(hand[0].rank);
       if (hand[i].rank === 11 || hand[i].rank === 12 || hand[i].rank === 13 ) {
         for (var j = 0; j < score.length; j++) {
          score[j] = score[j] + 10;
@@ -81,7 +83,7 @@ function handValue(hand) {
         score[0] += 11;
 
   }
-    console.log(score);
+    //console.log(score);
   }
   return score;
 }
@@ -103,13 +105,22 @@ function handValue(hand) {
 //     console.log(score);
 //   }
 // }
+function valueRound(round) {
+  for (var i = 0; i < round.length; i++) {
+    console.log(handValue(round[i]));
+  }
+}
 
-var currentHand = dealRound(shuffleArray(makeDeck()), 3);
-var deck = shuffleArray(makeDeck());
+var currentHand = dealRound(shuffle(makeDeck()), 3);
+var deck = shuffle(makeDeck());
 var hand = [];
-// handValue(dealRound(shuffleArray(makeDeck()), 1));
-hand.push(deck.shift());
-hand.push(deck.shift());
-handValue(hand);
+var round = dealRound(deck, 3);
 
+console.log(round);
+valueRound(round);
+
+// handValue(dealRound(shuffleArray(makeDeck()), 1));
+//hand.push(deck.shift());
+//hand.push(deck.shift());
+//handValue(hand);
 //console.log(dealRound(shuffleArray(makeDeck()), 3));
