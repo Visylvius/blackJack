36ded663b6de032515e57a9e776ceb4963aa0df9
Loren Barrick <lorenbarrick@gmail.com>
2016-05-13 19:29:49 -0700

    trying to get heroku to work

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
+console.log((path.join(__dirname, 'public')));
 var port = process.env.PORT || '3000';
 app.listen(port);
